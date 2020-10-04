374fcff6af4bd512128901234848609ae44f94a8
Minismuts <69870701+Minismuts@users.noreply.github.com>
2020-10-04 08:40:34 +0200

    Update Gang member.js

--- Gang member.js
+++ Gang member.js
@@ -1,5 +1,5 @@
 ////pScript made by naknemo#6161
-Cheat.Print("pSuedo script by naknemo");
+Cheat.Print("pScript by naknemo");
 UI.SetValue(["Misc.", "Helpers", "Client", "Reveal hidden cvars"], 1);
 UI.SetValue(["Visuals", "Chams", "Local", "Don't draw sleeves"], 1);
 Cheat.ExecuteCommand("r_skin 1");
