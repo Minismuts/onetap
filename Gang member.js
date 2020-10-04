9c63ab58260a9beaedaa36ca65a2ea4b6ab4fa72
Minismuts <69870701+Minismuts@users.noreply.github.com>
2020-10-04 02:09:19 +0200

    Add files via upload

---
+++ Gang member.js
@@ -0,0 +1,5 @@
+////pScript made by naknemo#6161
+Cheat.Print("pSuedo script by naknemo");
+UI.SetValue(["Misc.", "Helpers", "Client", "Reveal hidden cvars"], 1);
+UI.SetValue(["Visuals", "Chams", "Local", "Don't draw sleeves"], 1);
+Cheat.ExecuteCommand("r_skin 1");
